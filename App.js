2aae1b43f5da2529316718021fa49f715819b3c6
Abhinav <abhinav@parangat.com>
2018-10-16 13:20:11 +0530

    login

---
+++ App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import {
+    View,
+    StyleSheet,
+} from 'react-native';
+import LoginComponent from './src/app/component/login/login';
+
+export default class app extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <LoginComponent />
+            </View>
+        );
+    }
+}
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: 'skyblue',
+        flex: 1,
+    },
+})
---
+++ src/app/component/login/login.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+import {
+    Text,
+    Button,
+    Alert,
+    View,
+    StyleSheet,
+    AppRegistry,
+    TextInput
+} from 'react-native';
+
+import { name as appName } from '../../../../app.json';
+
+export default class LoginComponent extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { email: '', password: '' };
+    }
+    press() {
+        Alert.alert('You tapped the button!');
+    }
+    handleEmail(email) {
+        this.setState({ email: email })
+    };
+    handlePass(password) {
+        this.setState({ password: password })
+    };
+    render() {
+
+        return (
+            <View style={styles.container}>
+                <Text style={styles.v1}>Login</Text>
+                <View style={styles.loginBody}>
+                    <View style={{ padding: 10 }}>
+                        <TextInput
+                            style={{ height: 60 }}
+                            autoCapitalize="none"
+                            placeholder="Enter email!"
+                            onChangeText={(email) => this.handleEmail(email)}
+                        />
+                        <TextInput
+                            style={{ height: 60 }}
+                            autoCapitalize="none"
+                            placeholder="Enter password"
+                            onChangeText={(password) => this.handlePass(password)}
+                        />
+                    </View>
+                    <View style={{ padding: 10 }}>
+                        <Button onPress={() => { this.press() }}
+                            title='Login' />
+                    </View>
+                </View>
+            </View>
+        );
+    }
+}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    v1: {
+        textAlign: 'center',
+        padding: 15,
+        fontSize: 20,
+        backgroundColor: '#2741ff',
+        color: '#ffffff'
+    },
+    loginBody: {
+        padding: 10,
+        flex: 1,
+        justifyContent: 'center',
+    },
+})
+AppRegistry.registerComponent(appName, () => LoginComponent);
